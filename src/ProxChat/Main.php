68c705a23b4cf3d39f272b77a92a6f715320e495
DontTouchMeXD <76811635+DontTouchMeXD@users.noreply.github.com>
2020-12-31 18:33:54 +0700

    update Main file

--- src/ProxChat/Main.php
+++ src/ProxChat/Main.php
@@ -3,8 +3,23 @@
 namespace ProxChat;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Main extends PluginBase {
-
-
+    
+    /** @var Config $config */
+    public $config;
+    
+    public function onEnable(): void {
+        $pluginManager = $this->getServer()->getPluginManager();
+        $pluginManager->registerEvents(new EventListener($this), $this);
+        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
+        if($this->config->get("purechat_support")){
+            if($pluginManager->getPlugin("PureChat") === null){
+                $this->getLogger()->warning("can't found PureChat plugin, disabling plugin....");
+                $pluginManager->disablePlugin($this);
+            }
+        }
+    }
 }
+
