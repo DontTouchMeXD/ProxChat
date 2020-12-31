e4f3ecd21f711ac9e94daac66e021a26b1ccbc72
DontTouchMeXD <76811635+DontTouchMeXD@users.noreply.github.com>
2021-01-01 06:55:18 +0700

    cleaning code and fix bug

--- src/ProxChat/Main.php
+++ src/ProxChat/Main.php
@@ -11,36 +11,20 @@ class Main extends PluginBase {
     /** @var Config $config */
     public $config;
     
-    /** @var bool $isPureChatEnable */
-    private $isPureChatEnable = false;
-    
     public function onEnable(): void {
         $pluginManager = $this->getServer()->getPluginManager();
         $this->saveResource("config.yml");
         $pluginManager->registerEvents(new EventListener($this), $this);
         $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-        if($this->config->get("purechat_support")){
+        if($this->isPureChatEnable()){
             if($pluginManager->getPlugin("PureChat") === null){
                 $this->getLogger()->warning("can't found PureChat plugin, disabling plugin....");
                 $pluginManager->disablePlugin($this);
             }
-        } else {
-            $this->isPureChatEnable = true;
-        }
+        } 
     }
     
     public function isPureChatEnable(): bool {
-        return $this->isPureChatEnable;
-    }
-    
-    public function getPlayerNearby(Player $player): array {
-        $nearby = [];
-        foreach($player->getLevel()->getPlayers() as $p){
-            if($p->distance($player) <= $this->config->get("radius")){
-                $nearby[] = $p;
-            }
-        }
-        return $nearby;
+        return $this->config->get("purechat_support");
     }
 }
-
