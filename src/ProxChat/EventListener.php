fbc76d7725753c1a2535f9ebd9ccca19115a6572
DontTouchMeXD <76811635+DontTouchMeXD@users.noreply.github.com>
2021-01-01 06:54:18 +0700

    cleaning code and fix bug

--- src/ProxChat/EventListener.php
+++ src/ProxChat/EventListener.php
@@ -15,24 +15,28 @@ class EventListener implements Listener {
     }
     
     public function onChat(PlayerChatEvent $event){
-        if(!$event->isCancelled()){
-            return;
-        }
+        if($event->isCancelled()) return;
         $message = $event->getMessage();
         $player = $event->getPlayer(); 
-        $p = $this->main->getPlayerNearby($player);
         if($this->main->isPureChatEnable()){
             $pureChat = $this->main->getServer()->getPluginManager()->getPlugin("PureChat");
             $levelName = $pureChat->getConfig()->get("enable-multiworld-chat") ? $player->getLevel()->getName() : null;
             $originalChatFormat = $pureChat->getOriginalChatFormat($player, $levelName);
             $chatFormat = $pureChat->applyColors($originalChatFormat);
             $chatFormat = $pureChat->applyPCTags($chatFormat, $player, $message, $levelName);
-            $this->main->getServer()->broadcastMessage($chatFormat, $p); 
-            $event->setCancelled();
+            $event->setFormat($chatFormat);
         } else {
             $chatFormat = str_replace(["{username}", "{display_name}", "{message}"], [$player->getName(), $player->getDisplayName(), $message], $this->main->config->get("chat_format"));
-            $this->main->getServer()->broadcastMessage($chatFormat, $p);
-            $event->setCancelled();
+            $event->setFormat($chatFormat);
         }
+        $recipients = $event->getRecipients();
+		foreach($recipients as $key => $recipient){
+			if($recipient instanceof Player){
+				if($recipient->getLevel() != $player->getLevel() or $recipient->distance($player) > $this->main->config->get("radius")){
+				    unset($recipients[$key]); 
+				}
+			}
+		}
+		$event->setRecipients($recipients);  
     }
 }
